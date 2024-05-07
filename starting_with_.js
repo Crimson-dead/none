74d3295f3c829d1bc584329659573058a0e29a0d
Crimson-dead <165872505+Crimson-dead@users.noreply.github.com>
2024-05-07 19:11:17 +0200

    starting with js

---
+++ starting_with_.js
@@ -0,0 +1,21 @@
+function print(value) {
+	console.log(value);
+}
+print("Hello World!")
+a = 1
+c = true
+print("Let's go with for loop")
+for (let i = 0; i < 99; i += a) {
+	print(i)
+	print("a")
+	a = (i - a * -1) * a + i *2
+}
+try {
+	while True:
+		print("hehe ik it doesnt work here")
+}
+catch {
+	while (c) {
+		print(c)
+	}
+}

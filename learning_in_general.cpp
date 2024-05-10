552b44075fef798261cb8e988b532bf0973482b6
Crimson-dead <165872505+Crimson-dead@users.noreply.github.com>
2024-05-10 21:56:02 +0200

    c++ is weird
    "Hello World" program in c++

---
+++ learning_in_general.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+void print(char value)
+{
+    cout <<< value; // comments like in javascript
+}
+int main()
+{
+    print("Hello World!"); /* wait, there must be semicolons
+    c++ is really harder than python
+    python is better B)*/
+    return 0; // imo weird syntax
+    // what happens if it returns 1?
+}

beac36be4f968f0f752034d86f256cbc820047f9
Crimson-dead <165872505+Crimson-dead@users.noreply.github.com>
2024-05-11 22:29:32 +0200

    helloing to java
    Tomorrow – programming in java.

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+  public int square(int value) {
+    return value * value;
+  }
+  /*one
+  *two
+  *three
+  */
+  public static void main(String[] args){
+    System.out.print("Hello World, square of 5 is" + square(5)); // I think this code should work
+  }
+}

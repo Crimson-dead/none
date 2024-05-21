9eeaa8af1f8251f0d9833d6d639810060b38ea6b
Crimson-dead <165872505+Crimson-dead@users.noreply.github.com>
2024-05-21 14:26:43 +0200

    bored
    switching to my laptop

--- 🆗.py
+++ 🆗.py
@@ -1,1 +1,9 @@
-# Create
+"Anyone here?"
+# Anyone here?
+a = [0, ]
+f = lambda x: round(((x + 1) * 5) / 4)
+for j in range(5):
+    for i in a:
+        try: a.append(f(f(i) - j))
+        except: a.append(0)
+print(a)

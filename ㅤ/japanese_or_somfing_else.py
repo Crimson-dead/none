79c39e936cc14ed6b502f1d60edf17cea8afc775
Crimson-dead <165872505+Crimson-dead@users.noreply.github.com>
2024-05-28 20:10:00 +0200

    zukanokatokuariku_moshi_arimorinlukitoji_kutaariku.nofu

--- ㅤ/japanese_or_somfing_else.py
+++ ㅤ/japanese_or_somfing_else.py
@@ -1,5 +1,7 @@
-# e.g. C:\Users\example\directory> japanese_or_somfing_else.py Crimson-dead
-#      
+"""    
+e.g. C:\Users\example\directory> japanese_or_somfing_else.py Crimson-dead
+      mishikirinarimoto-tekukate
+"""
 from sys import argv
 
 try: name: str = argv[1]
@@ -9,6 +11,6 @@ final: list = []
 for i in name.lower():
     final.append({"a": "ka", "b": "zu", "c": "mi", "d": "te", "e": "ku", "f": "lu", "g": "ji", "h": "ri", "i": "ki",
                   "j": "zu", "k": "me", "l": "ta", "m": "rin", "n": "to", "o": "mo", "p": "no", "q": "ke", "r": "shi",
-                  "s": "ari", "t": "chi", "u": "do", "v": "ru", "w": "me", "x": "na", "y": "fu", "z": "zi", ...: ...}.get(i, "\u0001?\u0001"))
+                  "s": "ari", "t": "chi", "u": "do", "v": "ru", "w": "me", "x": "na", "y": "fu", "z": "zi", ...: ...}.get(i, i))
 input(f"{"".join(final)}\n")
  

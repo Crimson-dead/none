027438dc91af653238341b235203a310b574fe7b
Crimson-dead <165872505+Crimson-dead@users.noreply.github.com>
2024-06-07 22:25:19 +0200

    Update.py

--- ㅤ/.py
+++ ㅤ/.py
@@ -4,17 +4,16 @@
 even = lambda value: value % 2 == 0
 
 
-def func(value: int | float):
+def func(value: int | float) -> bool:
     # just int or float into bool function 
-    a = float()
+    a: float = float()
     for i in range((value % 5) * value % 12):
         a += i % 7
         a *= i % 3
-    return even(a)
+    return even(round(a))
 
 
-all = tuple(range(1000))
-passed = tuple(filter(func, all))
+all: tuple = tuple(range(1000))
+passed: tuple = tuple(filter(func, all))
 
 for j in passed: print(j)
- 
